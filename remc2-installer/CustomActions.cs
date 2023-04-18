4ed526bead2f6d726117c05460bc0bf169b7ea40
Timothy Hobbs <thobbsinteractive@gmail.com>
2023-04-18 22:35:09 +0200

    fix: changed solution to AnyCPU

--- remc2-installer/CustomActions.cs
+++ remc2-installer/CustomActions.cs
@@ -13,33 +13,25 @@ public class CustomActions
             enhancedTextures = true;
         }
 
-        if (enhancedTextures)
+
+        session.Log($"Setting Enhanced Textures to: {enhancedTextures}");
+        string path = session["INSTALLDIR"];
+        string configFilePath = Path.Combine(path, "config.ini");
+
+        session.Log($"Setting config.ini file: {configFilePath}");
+
+        if (System.IO.File.Exists(configFilePath))
         {
-            session.Log($"Installing Enhanced Textures...");
-
-            string path = session["INSTALLDIR"];
-
-            string configFilePath = Path.Combine(path, "config.ini");
-
-            session.Log($"Setting config.ini file: {configFilePath}");
-            if (System.IO.File.Exists(configFilePath))
-            {
-                session.Log($"Updating Ini File: {configFilePath}");
-                IniFile iniFile = new IniFile();
-                iniFile.Load(configFilePath);
-                iniFile["graphics"]["useEnhancedGraphics"] = "true ; if set to true, bigGraphicsFolder must be set as well";
-                iniFile.Save(configFilePath);
-                return ActionResult.Success;
-            }
-            else
-            {
-                return ActionResult.Failure;
-            }
+            session.Log($"Updating Ini File: {configFilePath}");
+            IniFile iniFile = new IniFile();
+            iniFile.Load(configFilePath);
+            iniFile["graphics"]["useEnhancedGraphics"] = $"{enhancedTextures} ; if set to true, bigGraphicsFolder must be set as well";
+            iniFile.Save(configFilePath);
+            return ActionResult.Success;
         }
         else
         {
-            session.Log($"User selected to not install Enchanced Textures");
-            return ActionResult.Success;
+            return ActionResult.Failure;
         }
     }
 }
