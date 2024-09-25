1aad8781bba888a10873d3184bf6eec938514cb0
Timothy Hobbs <thobbsinteractive@gmail.com>
2024-09-25 22:47:09 +0200

    feat: implemented more of the SoundDataFileClass

--- remc2/engine/SoundDataFile.cpp
+++ remc2/engine/SoundDataFile.cpp
@@ -1,11 +1,20 @@
 #include "SoundDataFile.h"
 
-SoundDataFile::SoundDataFile(std::string soundPath) : m_soundPath{ soundPath }
+SoundDataFile::SoundDataFile(std::string soundPath, int32_t sampleOffset) : m_soundPath{ soundPath }, m_sampleOffset { sampleOffset }
 {
 	FILE* ptrFile = DataFileIO::CreateOrOpenFile(soundPath.c_str(), 512);
 
 	LoadData(ptrFile);
 };
+
+SoundDataFile::~SoundDataFile()
+{
+	for (int i = 0; i < m_Samples.size(); i++)
+	{
+		delete m_Samples[i].data_44;
+	}
+	m_Samples.clear();
+}
 
 bool SoundDataFile::LoadData(FILE* ptrFile)
 {
@@ -13,8 +22,49 @@ bool SoundDataFile::LoadData(FILE* ptrFile)
 		return false;
 
 	DataFileIO::Seek(ptrFile, 0, 2);
-	long sizeOfFile = myftell(ptrFile);
-	DataFileIO::Seek(ptrFile, sizeOfFile - 4, 0);
+	m_fileSize = myftell(ptrFile);
+	DataFileIO::Seek(ptrFile, m_fileSize - 4, 0);
 	DataFileIO::Read(ptrFile, (uint8_t*)&m_bankStartIdex, 4);
+
+	LoadSamples(ptrFile, 0);
+
 	DataFileIO::Close(ptrFile);
+
+	return true;
+};
+
+bool SoundDataFile::LoadSamples(FILE* ptrFile, int32_t startOffset, int32_t samplePaddingBytes)
+{
+	int MAX_SAMPLES = 64;
+	while (MAX_SAMPLES)
+	{
+		startOffset = LoadSample(ptrFile, startOffset);
+		if (startOffset < 0)
+			break;
+
+		startOffset += samplePaddingBytes;
+		MAX_SAMPLES--;
+	}
+
+	return true;
+};
+
+int64_t SoundDataFile::LoadSample(FILE* ptrFile, int32_t startOffset)
+{
+	char fileType[] = { 'R','I','F','F' };
+	wav_t wavFile;
+	DataFileIO::Seek(ptrFile, startOffset, 0);
+	DataFileIO::Read(ptrFile, (uint8_t*)&wavFile, WAVE_HEADER_SIZE_BYTES);
+
+	wavFile.data_44 = new uint8_t[wavFile.dataSizeBytes_40];
+
+	if (!std::equal(std::begin(wavFile.isRiff_0), std::end(wavFile.isRiff_0), std::begin(fileType)) || wavFile.dataSizeBytes_40 < 1)
+		return -1;
+
+	DataFileIO::Seek(ptrFile, startOffset + WAVE_HEADER_SIZE_BYTES, 0);
+	DataFileIO::Read(ptrFile, wavFile.data_44, wavFile.dataSizeBytes_40);
+
+	m_Samples.push_back(wavFile);
+
+	return startOffset + WAVE_HEADER_SIZE_BYTES + wavFile.dataSizeBytes_40;
 };
--- remc2/engine/SoundDataFile.h
+++ remc2/engine/SoundDataFile.h
@@ -7,17 +7,24 @@
 #include <array>
 #include <thread>
 #include "defs.h"
+#include "../utilities/Wave.h"
 #include "../utilities/DataFileIO.h"
 
 class SoundDataFile
 {
 private:
 	std::string m_soundPath;
+	int64_t m_fileSize = 0;
 	int32_t m_bankStartIdex = 0;
+	int32_t m_sampleOffset = 90;
+	std::vector<wav_t> m_Samples;
 
 public:
-	SoundDataFile(std::string soundPath);
+	SoundDataFile(std::string soundPath, int32_t sampleOffset = 90);
+	~SoundDataFile();
 	bool LoadData(FILE* ptrFile);
+	bool LoadSamples(FILE* ptrFile, int32_t startOffset, int32_t samplePaddingBytes = 4);
+	int64_t LoadSample(FILE* ptrFile, int32_t startOffset);
 };
 
 #endif //SOUND_DATE_FILE
