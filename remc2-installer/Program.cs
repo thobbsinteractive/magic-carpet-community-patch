5ae2f65f3d46e85bdd45b0dac4fb456f3dccf695
Timothy Hobbs <thobbsinteractive@gmail.com>
2025-01-03 00:22:26 +0100

    fix: added ids for all files and directories

--- remc2-installer/Program.cs
+++ remc2-installer/Program.cs
@@ -15,15 +15,15 @@ namespace remc2_installer
 #else
 			var project = new ManagedProject("Magic Carpet 2 HD x86",
 #endif
-                             new Dir(@"%ProgramFiles%\ReMC\Magic Carpet 2 HD",
+                             new Dir(new Id("INSTALLDIR"), @"%ProgramFiles%\ReMC\Magic Carpet 2 HD",
                                  new DirPermission("Everyone", GenericPermission.All),
 #if WIN64
-                                 new File(@"..\x64\Release\remc2.exe", new FileShortcut("Magic Carpet 2 HD", @"%ProgramMenu%\ReMC\Magic Carpet 2 HD")
+                                 new WixSharp.File(new Id("PRODUCT_EXE"), @"..\x64\Release\remc2.exe", new FileShortcut(new Id("PRODUCT_EXE_SHORTCUT"), "Magic Carpet 2 HD", @"%ProgramMenu%\ReMC\Magic Carpet 2 HD")
                                  {
                                      IconFile = "Resources/app.ico"
                                  })
 #else
-                                 new WixSharp.File(@"..\Release\remc2.exe", new FileShortcut("Magic Carpet 2 HD", @"%ProgramMenu%\ReMC\Magic Carpet 2 HD")
+								 new WixSharp.File(new Id("PRODUCT_EXE"), @"..\Release\remc2.exe", new FileShortcut(new Id("PRODUCT_EXE_SHORTCUT"), "Magic Carpet 2 HD", @"%ProgramMenu%\ReMC\Magic Carpet 2 HD")
                                  {
                                      IconFile = "Resources/app.ico"
                                  })
@@ -37,12 +37,12 @@ namespace remc2_installer
                                     }
                                  },
 #if WIN64
-								 new File(@"..\x64\Release\remc2-editor.exe", new FileShortcut("Editor", @"%ProgramMenu%\ReMC\Magic Carpet 2 HD")
+								 new WixSharp.File(new Id("EDITOR_EXE"), @"..\x64\Release\remc2-editor.exe", new FileShortcut(new Id("EDITOR_EXE_SHORTCUT"), "Editor", @"%ProgramMenu%\ReMC\Magic Carpet 2 HD")
 								 {
 									 IconFile = "Resources/editor.ico"
 								 })
 #else
-                                 new WixSharp.File(@"..\Release\remc2-editor.exe", new FileShortcut("Editor", @"%ProgramMenu%\ReMC\Magic Carpet 2 HD")
+								 new WixSharp.File(new Id("EDITOR_EXE"), @"..\Release\remc2-editor.exe", new FileShortcut(new Id("EDITOR_EXE_SHORTCUT"), "Editor", @"%ProgramMenu%\ReMC\Magic Carpet 2 HD")
                                  {
                                      IconFile = "Resources/editor.ico"
                                  })
@@ -55,7 +55,7 @@ namespace remc2_installer
 										new FilePermission("CREATOR OWNER", GenericPermission.All)  { Execute = true },
 									}
 								 },
-								 new File(@"..\Release\config.ini")
+								 new File(new Id("CONFIG_INI"), @"..\Release\config.ini")
 								 {
 									 Permissions = new[] {
 										new FilePermission("Everyone", GenericPermission.All) { ChangePermission = true },
@@ -65,54 +65,54 @@ namespace remc2_installer
 									}
 								 },
 #if WIN64
-								 new File(@"..\x64\Release\brotlicommon.dll"),
-								 new File(@"..\x64\Release\brotlidec.dll"),
-								 new File(@"..\x64\Release\bz2.dll"),
-								 new File(@"..\x64\Release\fmt.dll"),
-								 new File(@"..\x64\Release\freetype.dll"),
-								 new File(@"..\x64\Release\libpng16.dll"),
-								 new File(@"..\x64\Release\ogg.dll"),
-								 new File(@"..\x64\Release\SDL2.dll"),
-								 new File(@"..\x64\Release\SDL2_image.dll"),
-								 new File(@"..\x64\Release\SDL2_mixer.dll"),
-								 new File(@"..\x64\Release\SDL2_ttf.dll"),
-								 new File(@"..\x64\Release\vorbis.dll"),
-								 new File(@"..\x64\Release\vorbisfile.dll"),
-								 new File(@"..\x64\Release\zlib1.dll"),
+								 new File(new Id("BROTLICOMMON_DLL"), @"..\x64\Release\brotlicommon.dll"),
+								 new File(new Id("BROTLIDEC_DLL"), @"..\x64\Release\brotlidec.dll"),
+								 new File(new Id("BZ2_DLL"), @"..\x64\Release\bz2.dll"),
+								 new File(new Id("FMT_DLL"),@"..\x64\Release\fmt.dll"),
+								 new File(new Id("FREETYPE_DLL"), @"..\x64\Release\freetype.dll"),
+								 new File(new Id("LIBPNG16_DLL"), @"..\x64\Release\libpng16.dll"),
+								 new File(new Id("OGG_DLL"), @"..\x64\Release\ogg.dll"),
+								 new File(new Id("SDL2_DLL"), @"..\x64\Release\SDL2.dll"),
+								 new File(new Id("SDL2_IMAGE_DLL"), @"..\x64\Release\SDL2_image.dll"),
+								 new File(new Id("SDL2_MIXER_DLL"), @"..\x64\Release\SDL2_mixer.dll"),
+								 new File(new Id("SDL2_TTF_DLL"), @"..\x64\Release\SDL2_ttf.dll"),
+								 new File(new Id("VORBIS_DLL"), @"..\x64\Release\vorbis.dll"),
+								 new File(new Id("VORBIXFILE_DLL"), @"..\x64\Release\vorbisfile.dll"),
+								 new File(new Id("ZLIB1_DLL"), @"..\x64\Release\zlib1.dll"),
 #else
-								 new File(@"..\Release\brotlicommon.dll"),
-								 new File(@"..\Release\brotlidec.dll"),
-								 new File(@"..\Release\bz2.dll"),
-								 new File(@"..\Release\fmt.dll"),
-								 new File(@"..\Release\freetype.dll"),
-								 new File(@"..\Release\libpng16.dll"),
-								 new File(@"..\Release\ogg.dll"),
-								 new File(@"..\Release\SDL2.dll"),
-								 new File(@"..\Release\SDL2_image.dll"),
-								 new File(@"..\Release\SDL2_mixer.dll"),
-								 new File(@"..\Release\SDL2_ttf.dll"),
-								 new File(@"..\Release\vorbis.dll"),
-								 new File(@"..\Release\vorbisfile.dll"),
-								 new File(@"..\Release\zlib1.dll"),
+								 new File(new Id("BROTLICOMMON_DLL"), @"..\Release\brotlicommon.dll"),
+								 new File(new Id("BROTLIDEC_DLL"), @"..\Release\brotlidec.dll"),
+								 new File(new Id("BZ2_DLL"), @"..\Release\bz2.dll"),
+								 new File(new Id("FMT_DLL"), @"..\Release\fmt.dll"),
+								 new File(new Id("FREETYPE_DLL"), @"..\Release\freetype.dll"),
+								 new File(new Id("LIBPNG16_DLL"), @"..\Release\libpng16.dll"),
+								 new File(new Id("OGG_DLL"), @"..\Release\ogg.dll"),
+								 new File(new Id("SDL2_DLL"), @"..\Release\SDL2.dll"),
+								 new File(new Id("SDL2_IMAGE_DLL"), @"..\Release\SDL2_image.dll"),
+								 new File(new Id("SDL2_MIXER_DLL"), @"..\Release\SDL2_mixer.dll"),
+								 new File(new Id("SDL2_TTF_DLL"), @"..\Release\SDL2_ttf.dll"),
+								 new File(new Id("VORBIS_DLL"), @"..\Release\vorbis.dll"),
+								 new File(new Id("VORBIXFILE_DLL"), @"..\Release\vorbisfile.dll"),
+								 new File(new Id("ZLIB1_DLL"), @"..\Release\zlib1.dll"),
 #endif
-								 new File(@"Extract.bat"),
-								 new Dir(@"kiss",
+								 new File(new Id("EXTRACT_BAT"), @"Extract.bat"),
+								 new Dir(new Id("KISS_INSTALLDIR"), @"kiss",
 									new Files(@"..\Release\kiss\*.*")),
-								 new Dir(@"font",
+								 new Dir(new Id("FONT_INSTALLDIR"), @"font",
                                     new Files(@"..\Release\font\*.*")),
-                                 new Dir(@"biggraphics",
+                                 new Dir(new Id("BIGGRAPHICS_INSTALLDIR"), @"biggraphics",
                                     new Files(@"..\enhancedassets\biggraphics\*.*")
                                     {
                                         ComponentCondition = "HIGHTEX=\"yes\""
                                     }),
-                                 new Dir(@"music-ogg",
+                                 new Dir(new Id("MUSICOGG_INSTALLDIR"), @"music-ogg",
                                     new Files(@"..\enhancedassets\music-ogg\*.*")),
-                                 new Dir(@"Extract",
-                                    new File(@"Extract\dosboxExtract-GOG-CD.conf"),
-                                    new File(@"Extract\XXCOPY16.EXE"),
-									new File(@"Extract\mpxplay.exe"))),
-                            new Property("HIGHTEX", "yes"),
-                            new ManagedAction(CustomActions.SetEnhancedTextures, Return.check, When.After, Step.InstallFinalize, Condition.NOT_Installed));
+                                 new Dir(new Id("EXTRACT_INSTALLDIR"), @"Extract",
+                                    new File(new Id("DOSBOXEXTRACT_CONF"), @"Extract\dosboxExtract-GOG-CD.conf"),
+                                    new File(new Id("XXCOPY16_EXE"), @"Extract\XXCOPY16.EXE"),
+									new File(new Id("MPXPLAY_EXE"), @"Extract\mpxplay.exe"))),
+                            new Property(new Id("HIGHTEX_PROPERTY"), "HIGHTEX", "yes"),
+                            new ManagedAction(new Id("MANAGED_ACTION"), CustomActions.SetEnhancedTextures, Return.check, When.After, Step.InstallFinalize, Condition.NOT_Installed));
 
 #if WIN64
             project.Platform = Platform.x64;
