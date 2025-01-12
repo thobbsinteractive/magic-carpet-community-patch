502126298ede7a8f6bd03608cd264c2fde78af00
Timothy Hobbs <thobbsinteractive@gmail.com>
2025-01-12 20:52:06 +0100

    fix: removed unecessary imports

--- remc2-editor/ReadConfig.h
+++ remc2-editor/ReadConfig.h
@@ -2,26 +2,11 @@
 #ifndef READCONFIG
 #define READCONFIG
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <vector>
 #include <filesystem>
-#include <cstdlib>
-
-#ifdef _MSC_VER
-    #include <direct.h>  
-    #include <io.h>  
-    #include <windows.h>
-#else
-    #include "dirent.h"
-#endif
 
 #include "../remc2/engine/CommandLineParser.h"
 #include "../remc2/portability/port_filesystem.h"
 #include "INIReader.h"
-#include "ini.h"
 
 class ReadConfig
 {
